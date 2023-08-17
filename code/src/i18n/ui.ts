1d09b690ccda19d50f813eeb90dfdf32c67f9d3b
Asier <asier.perez@ve.com>
2023-08-17 15:23:39 +0200

    [init] astro cc bidebieta webpage

---
+++ code/src/i18n/ui.ts
@@ -0,0 +1,20 @@
+export const languages = {
+    es: 'Castellano',
+    eu: 'Euskera',
+  };
+  
+  export const defaultLang = 'es';
+  
+  export const ui = {
+    en: {
+      'nav.home': 'Home',
+      'nav.about': 'About',
+      'nav.twitter': 'Twitter',
+    },
+    fr: {
+      'nav.home': 'Accueil',
+      'nav.about': 'À propos',
+    },
+  } as const;
+
+  export const showDefaultLang = false;
