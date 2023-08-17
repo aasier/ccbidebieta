5ec68d1778dafc1900bb2f47e4b6fd1fe0c82383
Asier <asier.perez@ve.com>
2023-08-17 22:58:25 +0200

    [lang] switcher

--- code/src/i18n/ui.ts
+++ code/src/i18n/ui.ts
@@ -1,20 +1,27 @@
 export const languages = {
-    es: 'Castellano',
-    eu: 'Euskera',
-  };
-  
-  export const defaultLang = 'es';
-  
-  export const ui = {
-    en: {
-      'nav.home': 'Home',
-      'nav.about': 'About',
-      'nav.twitter': 'Twitter',
-    },
-    fr: {
-      'nav.home': 'Accueil',
-      'nav.about': 'À propos',
-    },
-  } as const;
+  es: 'Castellano',
+  eu: 'Euskera',
+};
 
-  export const showDefaultLang = false;
+export const defaultLang = 'es';
+
+export const ui = {
+  es: {
+    'nav.home': 'Inicio',
+    'nav.concurso': 'Concurso KM',
+    'nav.salidas': 'Salidas y Escapadas',
+    'nav.donde': '¿De donde salimos?',
+    'nav.patrocinadores': 'Patrocinadores',
+    'nav.reglas': 'Regalmento interno'
+  },
+  eu: {
+    'nav.home': 'Hasiera',
+    'nav.concurso': 'KM Lehiaketa',
+    'nav.salidas': 'Irteerak eta Joan Etorriak',
+    'nav.donde': 'Nondik ateratzen gara?',
+    'nav.patrocinadores': 'Babesleak',
+    'nav.reglas': 'Barne Araudia'
+  },
+} as const;
+
+export const showDefaultLang = false;
